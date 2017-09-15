12d068a03ad128ca60ba60820e4f260f1a7ed144
John Gainfort Jr <john.gainfort@realeyes.com>
2017-09-14 23:21:38 -0600

    created initial driver

--- src/driver/hlsjs-driver.spec.ts
+++ src/driver/hlsjs-driver.spec.ts
@@ -3,13 +3,7 @@ import { makeHlsjsDriver } from './hlsjs-driver'
 
 describe('makeHlsDriver function', () => {
   it('should return a function', () => {
-    const driver = makeHlsjsDriver()
+    const driver = makeHlsjsDriver('video')
     expect(driver).to.be.a('function')
-  })
-
-  it('should return a function that returns a string with provided id', () => {
-    const driver = makeHlsjsDriver()
-    const result = driver('hls.js')
-    expect(result).equal('Hello hls.js')
   })
 })
--- src/driver/hlsjs-driver.ts
+++ src/driver/hlsjs-driver.ts
@@ -1,6 +1,20 @@
-export function makeHlsjsDriver(): (id: string) => string {
-  function hlsjsDriver(id: string): string {
-    return `Hello ${id}`
+import { adapt } from '@cycle/run/lib/adapt'
+import xs from 'xstream'
+import * as Hls from 'hls.js'
+
+export function makeHlsjsDriver(id: string, config?: Hls.OptionalConfig) {
+  if (!Hls.isSupported()) {
+    throw new Error('MSE not supported in your browser')
   }
+
+  const video = document.getElementById(id) as HTMLVideoElement
+  const hls = new Hls(config)
+
+  function hlsjsDriver(sink$) {
+    const source = xs.create()
+
+    return adapt(source)
+  }
+
   return hlsjsDriver
 }
